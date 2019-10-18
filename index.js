aa22a4778e2009d4be251807940d1b14786d7ab2
koder10001 <koder10001@gmail.com>
2019-10-17 19:13:30 -0500

    still not working yet LOL

--- index.js
+++ index.js
@@ -1,58 +1,66 @@
 #!/usr/bin/env node
 
 "use strict";
-const Flickr = require("flickrapi");
+const Flickr = require("flickr-sdk");
 const fs = require("fs");
 const inquirer = require("inquirer");
-const flickrOptions = {
+const flickrKeys = {
     api_key: "af1146a2df5582ec7a4b02c644eb2c1f",
     secret: "0ebfc840048d60b1",
-    user_id: '143988946@N05',
     access_token: '72157700484389675-24d7a90f926a3398',
     access_token_secret: '0096f537b1c3cfcf'
 };
-
+var flickr = new Flickr(Flickr.OAuth.createPlugin(flickrKeys.api_key,flickrKeys.secret,flickrKeys.access_token,flickrKeys.access_token_secret));
 
 console.clear();
 (async function(){
-    if(process.argv.length <= 2){
+    if(process.argv.length <= 2 || process.argv[2].trim() == ""){
         console.log("flickr <link>");
         process.exit();
     }
-    console.log("Authenticating ...");
     var __DIR__ = process.cwd();
-    let flickr = await Auth(flickrOptions);
-    if(!flickr){
-        console.log("error");
-        process.exit();
+    var url = process.argv[2];
+    var mode = (await inquirer.prompt([{
+        type: "list",
+        name: "reply",
+        message: "Download",
+        choices: ["photostream","album","favorite","gallery","group","all album"]
+    }])).reply;
+
+    switch(mode){
+        case "photostream":
+            let ID = await flickr.urls.lookupUser({url: url});
+            ID = ID.body.user.id;
+            console.log(ID);
+            let curPage = 1;
+            let res = "";
+            do {
+                res = await flickr.people.getPhotos({
+                    user_id: ID,
+                    per_page: 500,
+                    page: curPage++,
+                    extras: "url_sq,url_q,url_s,url_n,url_w,url_m,url_z,url_c,url_l,url_h,url_k,url_o"
+                })
+                console.log(res.body.photos.photo[0]);
+                // fs.writeFileSync(`photostream-${ID}.txt`,)
+            }while(curPage <= res.body.photos.pages);
+        break;
     }
-    let ans = await inquirer.prompt([
-        {
-            type:"list",
-            name:"aaaa",
-            message:"Download",
-            choices: [
-                "photostream",
-                "album",
-                "favorite",
-                "gallery"
-            ]
-        }
-    ]);
-    console.log(ans);
+
     process.exit();
 })();
 
-function Auth(flickrOptions){
-    return new Promise((resolve)=>{
-        Flickr.authenticate(flickrOptions,(error,flickr)=>{
-            if(error){
-                console.log(error);
-                resolve(false);
-            }
-            else {
-                resolve(flickr);
-            }
-        })
-    })
+function Largest(json){
+    
 }
+
+// function getLinks(method,id){
+//         method({
+//             user_id: id,
+//             group_id: id,
+//             gallery_id: id,
+//             per_page: 1,
+//             page:2,
+//             extras: "url_sq,url_q,url_s,url_n,url_w,url_m,url_z,url_c,url_l,url_h,url_k,url_o"
+//         });
+// }
